cb526c297236ed83c682db4608822f218afde911
leilaanastasiia <karpets.leilaanastasia@gmail>
2024-09-23 18:04:05 +0300

    triangle area added

--- apps/triangle_area.py
+++ apps/triangle_area.py
@@ -1,5 +1,8 @@
 def main():
-    print("triangle")
+    parameters = input('Please, enter an a & h of your triangle in cm (ex. 2 5): ')
+    a, h = parameters.strip().split(sep=' ')
+    area = 1/2 * int(a) * int(h)
+    print(f'The area of your triangle is {area} cm\u00b2.')
 
 
 if __name__ == '__main__':
