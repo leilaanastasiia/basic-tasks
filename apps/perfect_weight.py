57aca4674777086be95b4ec61ea0cb6a1ab461b0
leilaanastasiia <karpets.leilaanastasia@gmail>
2024-09-23 17:44:15 +0300

    perfect weight added

--- apps/perfect_weight.py
+++ apps/perfect_weight.py
@@ -1,5 +1,11 @@
 def main():
-    print("weight")
+    user_name: str = input('Please, enter your name: ')
+    user_height: str = input('Now enter your height in cm (ex. 165): ')
+    ideal_weight = int(user_height) - 110
+    if ideal_weight >= 0:
+        print(f'{user_name}, your perfect weight is {ideal_weight} kg.')
+    else:
+        print(f"{user_name}, your weight is already lower than optimal. Maybe, you should gain {abs(ideal_weight)} kg.")
 
 
 if __name__ == '__main__':
