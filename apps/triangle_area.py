07a2b9754fbf49445302610d973aee9f213b82f4
leilaanastasiia <karpets.leilaanastasia@gmail.com>
2024-09-26 14:20:24 +0300

    triangle area try-except :)

--- apps/triangle_area.py
+++ apps/triangle_area.py
@@ -1,8 +1,10 @@
 def main():
-    parameters = input('Please, enter an a & h of your triangle in cm (ex. 2 5): ')
-    a, h = parameters.strip().split(sep=' ')
-    area = 1/2 * int(a) * int(h)
-    print(f'The area of your triangle is {area} cm\u00b2.')
+    try:
+        base, height = input('Please, enter base, height of your triangle in cm (ex. 2 5): ').strip().split()
+        area = 0.5 * float(base) * float(height)
+        print(f'The area of your triangle is {area:.2f} cm\u00b2.')
+    except ValueError:
+        print("Please, enter 2 valid numbers.")
 
 
 if __name__ == '__main__':
