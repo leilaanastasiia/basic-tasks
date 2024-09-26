734852da53f06e254bd25205f292bf140e26fa59
leilaanastasiia <karpets.leilaanastasia@gmail.com>
2024-09-26 17:19:29 +0300

    right triangle more functioned

--- apps/right_triangle.py
+++ apps/right_triangle.py
@@ -1,15 +1,36 @@
+import math
+
+def get_parameters():
+    try:
+        user_input = input('Please, enter sides of your triangle in cm (ex. 10 10 14.14): ').strip().split()
+        parsed_data = [float(p) for p in user_input]
+        parsed_data.sort()
+        return parsed_data
+    except ValueError:
+        print("Please, enter 3 valid numbers ")
+        return None
+
+
+def is_equilateral_triangle(a, b, c):
+        return a == b == c
+
+def is_right_triangle(a, b, c, tolerance=0.01):
+    return math.isclose(a ** 2 + b ** 2, c ** 2, rel_tol=tolerance)
+
+def is_isosceles_triangle(a, b, c):
+    return a == b and a != c or b == c and b != a
+
+
 def main() -> None:
-    user_input = input('Please, enter sides of your triangle in cm (ex. 10 10 14.14): ')
-    parameters = [float(p) for p in user_input.strip().split(sep=' ')]
-    parameters.sort()
-    a, b, c = parameters[0], parameters[1], parameters[2]
-    if a == b == c:
+    parsed = get_parameters()
+    a, b, c = parsed[0], parsed[1], parsed[2]
+    if is_equilateral_triangle(a, b, c):
         print('This is an equilateral triangle.')
-    elif round(a * a + b * b) == round(c * c):
+    elif is_right_triangle(a, b, c):
         print('This is a right triangle.')
-        if a == b and a != c or b == c and b != a:
-            print('And an equilateral triangle as well.')
-    elif a == b and a != c or b == c and b != a:
+        if is_isosceles_triangle(a, b, c):
+            print('And an isosceles triangle as well.')
+    elif is_isosceles_triangle(a, b, c):
         print('This is an isosceles triangle.')
     else:
         print("This is not a right triangle.")
