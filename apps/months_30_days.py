eec1c502ac071398b3bbed869433baaeb94b78f6
leilaanastasiia <karpets.leilaanastasia@gmail.com>
2024-09-27 11:33:23 +0300

    make calendar func & dict comprehension

--- apps/months_30_days.py
+++ apps/months_30_days.py
@@ -1,16 +1,19 @@
-def main():
+def make_calendar() -> dict:
     months_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
                     'September', 'October', 'November', 'December']
     days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     calendar = dict((k, v) for k, v in zip(months_names, days_in_month))
-    output = []
+    return calendar
+
+
+
+def main():
+    calendar = make_calendar()
     days = input('Please enter a number of days in months of 2024 year: ')
-    for k, v in calendar.items():
-        if v == int(days):
-            output.append(k)
-    if len(output) == 0:
-        output = ['Sorry, no month with this value was found.']
-    print(output)
+    filtered = {k: v for k, v in calendar.items() if v == int(days)}
+    if len(filtered) == 0:
+        filtered = {'Result': 'Sorry, no month with this value was found.'}
+    print(*filtered.keys(), sep='\n')
 
 
 if __name__ == '__main__':
