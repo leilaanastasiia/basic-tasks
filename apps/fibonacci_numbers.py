59e553a02b4e5ed90a4f6ec2781e3aab9023b345
leilaanastasiia <karpets.leilaanastasia@gmail.com>
2024-09-27 11:39:12 +0300

    fibonacci numbers separated from main

--- apps/fibonacci_numbers.py
+++ apps/fibonacci_numbers.py
@@ -1,10 +1,13 @@
-def main():
-    stop = input('Please, tell me where to stop a fibonacci sequence, '
-        'because we all need to stop somewhere... (ex. 100): ')
+def fibonacci_sequence(stop: int) -> list:
     fibonacci_numbers = [1, 1]
     for i in range(2, int(stop)):
         fibonacci_numbers.append(fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2])
-    print(fibonacci_numbers)
+    return fibonacci_numbers
+
+def main():
+    stop = input('Please, tell me where to stop a fibonacci sequence, '
+        'because we all need to stop somewhere... (ex. 100): ')
+    print(fibonacci_sequence(int(stop)))
 
 if __name__ == '__main__':
     main()
